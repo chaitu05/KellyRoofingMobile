681e1dfe980dac385dbb5a93077722738c2655e2
kumar m <cmynam@gmail.com>
2017-09-15 16:44:23 -0600

    Initial commit.

--- app/model/order-confirmation.ts
+++ app/model/order-confirmation.ts
@@ -1,9 +1,11 @@
 export class OrderConfirmation {
     confirmed: boolean;
+    confirmedAt: Date;
     priorDays: number;
     public toString = () => {
         return `Order confirmation = (${
         "\nConfirmed = " + this.confirmed
+        + "\nConfirmed at = " + this.confirmedAt
         + "\nPrior to days = " + this.priorDays
             })`
     }
